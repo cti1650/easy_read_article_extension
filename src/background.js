32d778d42d41fca826e0c1e5cc5a65a719fad78e
cti1650 <cti1650@gmail.com>
2022-05-21 23:13:55 +0900

    feat: 基本機能実装完了

--- src/background.js
+++ src/background.js
@@ -1,11 +1,12 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (!changeInfo.status) return;
   let url = tab.url;
-  if (!~url.indexOf('dev.com')) return;
-  chrome.scripting.excuteScript({
+  console.log(url)
+  if (!~url.indexOf('dev.to')) return;
+  chrome.scripting.executeScript({
     target: {
       'tabId': tabId,
-      'files': ['script.js']
-    }
+    },
+    'files': ['script.js']
   })
 });
--- src/script.js
+++ src/script.js
@@ -1,11 +1,21 @@
-document.querySelectorAll("article p").forEach(item => {
-    if (!item.querySelector('img')) {
-      const text = item.textContent
-      const result = text.split(' ').map(word => {
-        if (word.length > 2) {
-          return `<b>${word.slice(0,2)}</b>${word.slice(2)}`
+console.log("done");
+document.querySelectorAll("article p").forEach((item) => {
+  if (!item.querySelector("img")) {
+    const text = item.textContent;
+    const result = text
+      .split(" ")
+      .map((word) => {
+        if (word.length === 2) {
+          return `<b>${word.slice(0, 1)}</b>${word.slice(1)}`;
+        } else if (word.length === 3) {
+          return `<b>${word.slice(0, 1)}</b>${word.slice(1)}`;
+        } else if (word.length === 4) {
+          return `<b>${word.slice(0, 3)}</b>${word.slice(3)}`;
+        } else if (word.length > 4) {
+          return `<b>${word.slice(0, 4)}</b>${word.slice(4)}`;
         }
-      }).join(' ')
-      item.innerHTML = result
-    }
-  })
+      })
+      .join(" ");
+    item.innerHTML = result;
+  }
+});
