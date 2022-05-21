70e937fd60cab8a2becf0d0ab15d132e01a31f83
cti1650 <cti1650@gmail.com>
2022-05-22 00:30:42 +0900

    fix: 記号の文字化けとスペース文字コード違いによる動作エラーに対応

--- src/script.js
+++ src/script.js
@@ -1,21 +1,31 @@
 console.log("done");
-document.querySelectorAll("article p").forEach((item) => {
-  if (!item.querySelector("img")) {
-    const text = item.textContent;
-    const result = text
-      .split(" ")
-      .map((word) => {
-        if (word.length === 2) {
-          return `<b>${word.slice(0, 1)}</b>${word.slice(1)}`;
-        } else if (word.length === 3) {
-          return `<b>${word.slice(0, 1)}</b>${word.slice(1)}`;
-        } else if (word.length === 4) {
-          return `<b>${word.slice(0, 3)}</b>${word.slice(3)}`;
-        } else if (word.length > 4) {
-          return `<b>${word.slice(0, 4)}</b>${word.slice(4)}`;
-        }
-      })
-      .join(" ");
-    item.innerHTML = result;
-  }
-});
+(() => {
+  const checkword = (word) => {
+    const reg = /^[a-zA-Z0-9,\.'\!\?]+$/;
+    return reg.test(word);
+  };
+  document.querySelectorAll("article p,article li,article ui").forEach((item) => {
+    if (!item.querySelector("img")) {
+      const text = item.textContent;
+      const result = text
+        .split(/\s+/)
+        .map((word) => {
+          if (checkword(word)) {
+            if (word.length === 2) {
+              return `<b>${word.slice(0, 1)}</b>${word.slice(1)}`;
+            } else if (word.length === 3) {
+              return `<b>${word.slice(0, 1)}</b>${word.slice(1)}`;
+            } else if (word.length === 4) {
+              return `<b>${word.slice(0, 3)}</b>${word.slice(3)}`;
+            } else if (word.length > 4) {
+              return `<b>${word.slice(0, 4)}</b>${word.slice(4)}`;
+            }
+          } else {
+            return word;
+          }
+        })
+        .join(" ");
+      item.innerHTML = result;
+    }
+  });
+})();
